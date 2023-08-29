5ac7b6e9005a1cb62c514a536446f2404559f06a
Cristian Teletor <cristianteletor@Cristians-MacBook-Air.local>
2023-08-29 18:56:18 +0200

    trying to do a skeleton effect on my rick and morty app

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Navbar from './Navbar';
 import Sites from "./Sites";
 import Character from './Character';
 import './App.css';
+import Skeleton from "./component/Skeleton";
 
 function App() {
   return (
     <>
+   
       <Navbar />
       <Routes>
         <Route path="/episode" element={<Episode />} />
         <Route path="/sites" element={<Sites />} />
         <Route path="/character" element={<Character />} />
       </Routes>
+      
     </>
 
   );
--- src/Character.js
+++ src/Character.js
@@ -1,40 +1,75 @@
+import { useState } from "react";
 import useSWR from "swr";
+import Skeleton from "./component/Skeleton";
 
+const fetcher = async (url) => fetch(url).then((res) => res.json());
 const Character = () => {
-  const fetcher = async (url) => fetch(url).then((res) => res.json());
+  const [page, setPage] = useState(1);
   const { data, error, isLoading } = useSWR(
-    "https://rickandmortyapi.com/api/character",
+    `https://rickandmortyapi.com/api/character/?page=${page}`,
     fetcher
   );
-  console.log(data);
+  const [imageLoaded, setImageLoaded] = useState(true);
 
   return (
     <>
-      <div className="w-full flex flex-wrap justify-center">
-        {data?.results?.map((hello) => {
-          console.log("photos", hello);
-          return (
-            <>
-              <div
-                key={hello?.id}
-                className="h-[300px] justify-start items-center flex-col  flex"
-              >
-                <div className="w-[289px] h-[230px]  flex">
-                  <img
-                    className="w-[289px] h-[230px] rounded-lg border border-blue-800 border-2 mr-5 "
-                    src={hello.image}
-                    alt={hello.name}
-                  />
-                </div>
-                <div>
-                <h1>{hello.name}</h1>
-                <button className="text-white bg-blue-600 w-20 h-10 rounded-md">See More</button>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
+      {isLoading ? (
+        "loadingggg"
+      ) : (
+        <>
+          <div className="w-full flex flex-wrap justify-center">
+            {data?.results?.map((characters) => {
+              console.log("imagesssss", characters.image);
+              return (
+                <>
+                  <div
+                    key={characters?.id}
+                    className="h-[300px] justify-start items-center flex-col  flex max-w-sm"
+                  >
+                    {imageLoaded ? <Skeleton /> : 
+                    <>
+                    <div className="w-[289px] h-[230px] flex">
+                      <img
+                        onLoad={() => setImageLoaded(true)}
+                        className="w-[289px] h-[230px] rounded-lg border border-blue-800  mr-5 rounded-full rounded-lg bg-rose-100/10"
+                        src={characters.image}
+                        alt={characters.name}
+                        loading="lazy"
+                      />
+                    </div>
+                    </>
+                    }
+                    
+                    
+                    <div>
+                      <h1>{characters.name}</h1>
+                      <button className="text-white bg-blue-600 w-20 h-10 rounded-md">
+                        See More
+                      </button>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          <div className="w-[100%] flex justify-evenly justify-center items-center h-[100px]">
+            <button
+              onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+              disabled={page === 1}
+              className="bg-blue-200 w-[10%]"
+            >
+              back
+            </button>
+            <button
+              className="bg-blue-200 w-[10%]"
+              onClick={() => setPage((prev) => prev + 1)}
+              disabled={!data?.info?.next}
+            >
+              next
+            </button>
+          </div>
+        </>
+      )}
     </>
   );
 };
